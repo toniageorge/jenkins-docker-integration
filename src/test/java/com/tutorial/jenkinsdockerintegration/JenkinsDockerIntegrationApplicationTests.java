643a8ead1b15b9030fded76cfa0479264829e29d
tonia <toniageorge@gmail.com>
2024-02-27 02:22:42 +0000

    docker integration

---
+++ src/test/java/com/tutorial/jenkinsdockerintegration/JenkinsDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.jenkinsdockerintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
